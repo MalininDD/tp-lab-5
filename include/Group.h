24529c9f0327734f5554f148cfdffe71394e0f64
MalininDD <54958481+MalininDD@users.noreply.github.com>
2021-02-05 20:12:15 +0300

    Add files via upload

---
+++ include/Group.h
@@ -0,0 +1,48 @@
+// Copyright 2021 MalininDmitry
+#ifndef INCLUDE_GROUP_H_
+#define INCLUDE_GROUP_H_
+
+#include <string>
+#include <vector>
+
+class Student;
+
+class Group{
+private:
+    std::string title;
+    std::string spec;
+    std::vector<Student*> students;
+    Student * head;
+    
+public:
+    Group(std::string title, std::string spec);
+    
+    Group(std::string title);
+    
+    void addStudent(Student * student);
+    
+    void chooseHead(Student * student);
+    
+    void chooseHead();
+    
+    void removeStudent(Student * student);
+    
+    double getAverageMark();
+    
+    Student * containsStudent(int num);
+    
+    Student * containsStudent(std::string fio);
+    
+    int countStudent();
+    
+    std::vector<Student*> listStudent();
+    
+    std::string getSpec();
+    
+    std::string getTitle();
+    
+    bool isEmpty();
+};
+
+#endif  // INCLUDE_GROUP_H_
+
---
+++ include/Student.h
@@ -0,0 +1,38 @@
+// Copyright 2021 MalininDmitry
+#ifndef INCLUDE_STUDENT_H_
+#define INCLUDE_STUDENT_H_
+
+#include <string>
+#include <vector>
+
+class Group;
+
+class Student{
+private:
+    int id;
+    std::string fio;
+    Group * group;
+    std::vector<int> marks;
+    
+public:
+    Student(int id, std::string fio);
+    
+    ~Student();
+    
+    double getAveragemark();
+    
+    int getId();
+    
+    std::string getFio();
+    
+    void addToGroup(Group * groupName);
+    
+    void addMark(int mark);
+    
+    void changeId(int index);
+    
+    Group * getGroup();
+};
+
+#endif  // INCLUDE_STUDENT_H_
+
