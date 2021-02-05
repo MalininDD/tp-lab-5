6d18c9224c68de68d4f2e08ac30b9e3a10f2811f
MalininDD <54958481+MalininDD@users.noreply.github.com>
2021-02-05 20:12:47 +0300

    Add files via upload

---
+++ src/Deanery.cpp
@@ -0,0 +1,132 @@
+// Copyright 2021 MalininDmitry
+#include "Group.h"
+#include "Student.h"
+#include "Deanery.h"
+#include <random>
+#include <iostream>
+
+
+void Deanery::createGroups(std::string filename){
+    std::string input;
+    std::string spec;
+    std::string title;
+    std::ifstream file(filename);
+    int i = 0;
+    while (getline(file,input)) {
+        int pos = input.find(':');
+        spec = input.substr(0, pos);
+        title = input.substr(pos + 1, input.size() - pos - 1);
+        Group* g = new Group(title, spec);
+        i++;
+        groups.push_back(g);
+    }
+    file.close();
+}
+
+void Deanery::initHeads(){
+    for(auto g : groups){
+        g->chooseHead();
+    }
+}
+
+Deanery::Deanery(std::string filename){
+    createGroups(filename);
+    initHeads();
+}
+
+void Deanery::hireStudents(std::string filename){
+    std::string input;
+    std::ifstream file(filename);
+    int index = 0;
+    srand(0);
+    while (getline(file,input)) {
+        Student* stud = new Student(0, input);
+        index = rand() % (this->groups.size());
+        stud->changeId(this->groups[index]->countStudent());
+        this->groups[index]->addStudent(stud);
+    }
+    
+    file.close();
+}
+
+void Deanery::addMarksToAll(int mark){
+    for (int i = 0; i < groups.size(); i++) {
+        for (int j = 0; j < groups[i]->countStudent(); j++) {
+            groups[i]->listStudent()[j]->addMark(mark);
+        }
+    }
+}
+
+void Deanery::getStatistics(std::string filename){
+    std::ofstream out;
+    out.open(filename);
+    out << "Groups :" << std::endl;
+    
+    for (int i = 0; i < this->groups.size(); i++) {
+        out << this->groups[i]->getTitle()
+        << " : " << this->groups[i]->getAverageMark()
+        << std::endl;
+    }
+    out << "Students :" << std::endl;
+    
+    for (int i = 0; i < this->groups.size(); i++) {
+        for (int j = 0;
+             j < this->groups[i]->listStudent().size();
+             j++) {
+            out <<
+            this->groups[i]->listStudent()[j]->getFio()
+            << " : " <<
+            this->groups[i]->listStudent()[j]->getAveragemark()
+            << std::endl;
+        }
+    }
+    
+    out.close();
+}
+
+void Deanery::moveStudents(Student * student, Group * group) {
+    student->getGroup()->removeStudent(student);
+    group->addStudent(student);
+}
+
+void Deanery::fireStudents() {
+    for (auto g : groups) {
+        auto gg = g->listStudent();
+        for (auto s : gg) {
+            if(s->getAveragemark() < 3.5 )
+                g->removeStudent(s);
+        }
+    }
+}
+
+void Deanery::saveStaff(std::string filename_group, std::string filename_students){
+    std::ofstream fgroup(filename_group);
+    std::ofstream fstudent(filename_students);
+    
+    for (auto g : groups) {
+        fgroup << g->getTitle() << ":"
+        << g->getSpec() << std::endl;
+        auto gg = g->listStudent();
+        for (auto s : gg) {
+            fstudent << s->getFio()
+            << std::endl;
+        }
+    }
+    
+    fgroup.close();
+    fstudent.close();
+}
+
+void Deanery::outputToConsole(){
+    for (auto g : groups) {
+        std::cout << g->getTitle() << " : "
+        << g->getSpec() << " - "
+        << g->getAverageMark() << std::endl;
+        auto gg = g->listStudent();
+        for (auto s : gg) {
+            std::cout << s->getFio() << " - " <<
+            s->getAveragemark()
+            << std::endl;
+        }
+    }
+}
---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+// Copyright 2021 MalininDmitry
+#include <iostream>
+#include <string>
+#include "Student.h"
+#include "Group.h"
+#include "Deanery.h"
+
+
+int main() {
+    unsigned int id = 11;
+    std::string fio = "Ddsa Sfdsfsdf";
+    Student student1(id, fio);
+    unsigned int id2 = 12;
+    std::string fio2 = "Ddsa Sfdsfsd2332";
+    Student student2(id2, fio2);
+    std::string spec = "Color";
+    std::string title = "Red";
+    Group group(title, spec);
+    group.addStudent(&student1);
+    group.addStudent(&student2);
+    std::cout << group.isEmpty() << std::endl;
+    
+    Deanery deanery("DataGroup.txt");
+    deanery.hireStudents("DataStudent.txt");
+    deanery.addMarksToAll(4);
+    deanery.addMarksToAll(5);
+    deanery.outputToConsole();
+
+    deanery.fireStudents();
+    deanery.outputToConsole();
+    
+    deanery.getStatistics("stat.txt");
+    deanery.saveStaff("DataGroup.txt", "DataStudent.txt");
+    
+    return 0;
+}
